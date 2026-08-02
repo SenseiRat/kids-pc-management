28a1e09d4a852ac131ab96062a1c6664ca244c1b
Sean <sean@starnes.cloud>
2026-08-02 11:27:25 -0500

    Adding in commented out verbosity

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,28 +5,30 @@ MODE="$2"
 TAGS=("${@:3}")
 
 COMMON_ARGS=(
-    --inventory inventory
-    --user ansible
-    --private-key .ansible_private_key
-    --vault-password-file .ansible_vault_pass
-    sites.yml
+  --inventory inventory
+  --user ansible
+  --private-key .ansible_private_key
+  --vault-password-file .ansible_vault_pass
+  sites.yml
+  ### -vvv
 )
 
 if [[ "$ACTION" == "check" ]]; then
-    COMMON_ARGS+=(--check --diff)
+  COMMON_ARGS+=(--check --diff)
 elif [[ "$ACTION" != "run" ]]; then
-    echo "Usage: $0 <check|run> [limit|skip <tags...>]"
-    exit 1
+  echo "Usage: $0 <check|run> [limit|skip <tags...>]"
+  exit 1
 fi
 
 if [[ "$MODE" == "limit" ]]; then
-    IFS=,
-    COMMON_ARGS+=(--tags "${TAGS[*]}")
-    unset IFS
+  IFS=,
+  COMMON_ARGS+=(--tags "${TAGS[*]}")
+  unset IFS
 elif [[ "$MODE" == "skip" ]]; then
-    IFS=,
-    COMMON_ARGS+=(--skip-tags "${TAGS[*]}")
-    unset IFS
+  IFS=,
+  COMMON_ARGS+=(--skip-tags "${TAGS[*]}")
+  unset IFS
 fi
 
 ansible-playbook "${COMMON_ARGS[@]}"
+
